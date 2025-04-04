962ea5d90ed970d6d084a81ed853f6329e8210a8
EC2 Default User <ec2-user@ip-172-31-8-143.ap-northeast-1.compute.internal>
2025-04-04 06:23:12 +0000

    [Add] routeの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,33 @@
 Rails.application.routes.draw do
-  get 'comments/create'
-  get 'comments/destroy'
-  get 'reviews/new'
-  get 'reviews/index'
-  get 'reviews/show'
-  get 'reviews/create'
-  get 'reviews/edit'
-  get 'reviews/update'
-  get 'reviews/destroy'
-  get 'users/edit'
-  get 'users/show'
-  get 'users/update'
-  get 'users/destroy'
-  get 'users/confirm_deletion'
-  get 'homes/top'
-  get 'homes/about'
+
   devise_for :users
+
+  # ユーザールーティング
+  root "homes#top"
+  get "about", to: "homes#about"
+
+  get 'users/confirm_deletion', to: "users#confirm_deletion"
+  resources :users, only: [
+    :edit,
+    :show,
+    :update,
+    :destroy
+  ]
+
+  resources :reviews, only: [
+    :new,
+    :index,
+    :show,
+    :create,
+    :edit,
+    :update,
+    :destroy
+  ] do
+    resources :comments, only: [
+      :create,
+      :destroy
+    ]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
