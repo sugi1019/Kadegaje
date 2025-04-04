e47eb3bd1cd3ddf7c37c50941d88f8444a785761
EC2 Default User <ec2-user@ip-172-31-8-143.ap-northeast-1.compute.internal>
2025-04-04 05:09:42 +0000

    [Add] controller view追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def confirm_deletion
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'comments/create'
+  get 'comments/destroy'
+  get 'reviews/new'
+  get 'reviews/index'
+  get 'reviews/show'
+  get 'reviews/create'
+  get 'reviews/edit'
+  get 'reviews/update'
+  get 'reviews/destroy'
+  get 'users/edit'
+  get 'users/show'
+  get 'users/update'
+  get 'users/destroy'
+  get 'users/confirm_deletion'
+  get 'homes/top'
+  get 'homes/about'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
