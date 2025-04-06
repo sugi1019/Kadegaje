ab496e45d6e745d7029f9d19bb8459deb9725c5a
EC2 Default User <ec2-user@ip-172-31-8-143.ap-northeast-1.compute.internal>
2025-04-06 12:21:43 +0000

    [Add] seedの追加途中

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,37 +22,37 @@ User.create!(
   email: "a@a",
   password: "aaaaaa"
 )
-　　　　　　　　　　　　　　　　　　　　　　# user.image.attach(io: File.open(Rails.root.join('app/assets/images/hoge,jpg')),filename: 'hoge.jpg')
+user.image.attach(io: File.open(Rails.root.join('app/assets/images/no_image.jpg')),filename: 'no_image.jpg')
 # ユーザー情報2
 User.create!(
   display_name: "testB",
   email: "b@b",
   password: "bbbbbb"
 )
-# user.image.attach(io: File.open(Rails.root.join('app/assets/images/hoge,jpg')),filename: 'hoge.jpg')
+user.image.attach(io: File.open(Rails.root.join('app/assets/images/user_sample_icon.jpg')),filename: 'user_sample_icon.jpg')
 
 
-ジャンル情報
+# ジャンル情報
 Genre.create!(name: "キッチン家電・ガジェット")
-　　　　　　　　　　　　　　　　　　　　　　# genre.image.attach(io: File.open(Rails.root.join('app/assets/images/hoge,jpg')),filename: 'hoge.jpg')
+genre.image.attach(io: File.open(Rails.root.join('app/assets/images/genre_icon1.jpg')),filename: 'genre_icon1.jpg')
 Genre.create!(name: "生活家電")
-# genre.image.attach(io: File.open(Rails.root.join('app/assets/images/hoge,jpg')),filename: 'hoge.jpg')
+genre.image.attach(io: File.open(Rails.root.join('app/assets/images/genre_icon2.jpg')),filename: 'genre_icon2.jpg')
 Genre.create!(name: "オーディオ・音響機器")
-# genre.image.attach(io: File.open(Rails.root.join('app/assets/images/hoge,jpg')),filename: 'hoge.jpg')
+genre.image.attach(io: File.open(Rails.root.join('app/assets/images/genre_icon3.jpg')),filename: 'genre_icon3.jpg')
 Genre.create!(name: "パソコン・周辺機器")
-# genre.image.attach(io: File.open(Rails.root.join('app/assets/images/hoge,jpg')),filename: 'hoge.jpg')
-Genre.create!(name: "スマートフォン・タブレット")
-# genre.image.attach(io: File.open(Rails.root.join('app/assets/images/hoge,jpg')),filename: 'hoge.jpg')
+genre.image.attach(io: File.open(Rails.root.join('app/assets/images/genre_icon4.jpg')),filename: 'genre_icon4.jpg')
+Genre.create!(name: "スマホタブレット・周辺機器")
+genre.image.attach(io: File.open(Rails.root.join('app/assets/images/genre_icon5.jpg')),filename: 'genre_icon5.jpg')
 Genre.create!(name: "カメラ・ビデオカメラ")
-# genre.image.attach(io: File.open(Rails.root.join('app/assets/images/hoge,jpg')),filename: 'hoge.jpg')
+genre.image.attach(io: File.open(Rails.root.join('app/assets/images/genre_icon6.jpg')),filename: 'genre_icon6.jpg')
 Genre.create!(name: "ヘルスケア・フィットネスガジェット")
-# genre.image.attach(io: File.open(Rails.root.join('app/assets/images/hoge,jpg')),filename: 'hoge.jpg')
+genre.image.attach(io: File.open(Rails.root.join('app/assets/images/genre_icon7.jpg')),filename: 'genre_icon7.jpg')
 Genre.create!(name: "ゲーム機・ゲームアクセサリー")
-# genre.image.attach(io: File.open(Rails.root.join('app/assets/images/hoge,jpg')),filename: 'hoge.jpg')
+genre.image.attach(io: File.open(Rails.root.join('app/assets/images/genre_icon8.jpg')),filename: 'genre_icon8.jpg')
 Genre.create!(name: "DIY・工具ガジェット")
-# genre.image.attach(io: File.open(Rails.root.join('app/assets/images/hoge,jpg')),filename: 'hoge.jpg')
+genre.image.attach(io: File.open(Rails.root.join('app/assets/images/genre_icon9.jpg')),filename: 'genre_icon9.jpg')
 Genre.create!(name: "旅行・アウトドアガジェット")
-# genre.image.attach(io: File.open(Rails.root.join('app/assets/images/hoge,jpg')),filename: 'hoge.jpg')
+genre.image.attach(io: File.open(Rails.root.join('app/assets/images/genre_icon10.jpg')),filename: 'genre_icon10.jpg')
 
 
 # レビュー情報
@@ -62,27 +62,27 @@ Review.create!(
   title: "お掃除ロボット買ったよ～",
   rating: 5,#integer ✩5評価
   post: "全自動お掃除ロボット（◯◯バ）一ヶ月使ってみたよ～\n仕事から帰っ来た時に髪の毛1本床に落ちてない環境に大満足!!\nみんなも使ってみてね～"
-  # product_name: "◯◯製xxxx"
+  product_name: "◯◯製xxxx"
 )
-　　　　　　　　　　　　　　　　　　　　　　　　　# review.image.attach(io: File.open(Rails.root.join('app/assets/images/hoge,jpg')),filename: 'hoge.jpg')
+　　　　　　　　　　　　　　　　　　　　　　　　　# review.image.attach(io: File.open(Rails.root.join('app/assets/images/hoge.jpg')),filename: 'hoge.jpg')
 Review.create!(
   user_id: 2,
   genre_id: 4, #パソコン・周辺機器
   title: "お気に入りのキーボード",
   rating: 4,#integer ✩5評価
   post: "おしゃれなキーボード買いました。クリアカラーで写真映えする！\nただ、もう少し音が静かなら良かったかなと思いました。"
-  # product_name: "◯◯製xxxx"
+  product_name: "◯◯製xxxx"
 )
-# review.image.attach(io: File.open(Rails.root.join('app/assets/images/hoge,jpg')),filename: 'hoge.jpg')
+# review.image.attach(io: File.open(Rails.root.join('app/assets/images/hoge.jpg')),filename: 'hoge.jpg')
 Review.create!(
   user_id: 3,
   genre_id: 1, #キッチン家電・ガジェット
   title: "最高のコーヒープレスを入手した",
   rating: 5,#integer ✩5評価
   post: "僕はコーヒーが好きでほぼ毎日入れるんだけど、いままでは忙しい朝にハンドドリップしてたんだけど流石に時間がなくて...\n時短のために〇〇のコーヒープレス使ってみたら大当たり！片付け簡単でおすすめです！"
-  # product_name: "◯◯製"
+  product_name: "◯◯製"
 )
-# review.image.attach(io: File.open(Rails.root.join('app/assets/images/hoge,jpg')),filename: 'hoge.jpg')
+# review.image.attach(io: File.open(Rails.root.join('app/assets/images/hoge.jpg')),filename: 'hoge.jpg')
 
 
 
