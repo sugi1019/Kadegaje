b495c994f0b8cfdcc36729673e58dab0eb3443e5
EC2 Default User <hisayori.sugimoto@gmail.com>
2025-04-08 10:23:31 +0000

    [Add] user新規登録時デフォルトアイコンを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,19 @@ class User < ApplicationRecord
   # 表示名は2文字以上必須
   validates :display_name, presence: true, length: {minimum: 2}
 
+  # 新規作成直後に実行
+  after_create :users_image_default
+
+  private
+
+  # 新規作成時にデフォルトアイコンを設定
+  def users_image_default
+    # 画像が存在するか確認
+    unless users_image.attached?
+      users_image.attach(io: File.open(Rails.root.join("public", "images", "no_image.jpg")),
+      filename: "no_image.jpg",
+      content_type: "image/jpeg"
+      )
+    end
+  end
 end
