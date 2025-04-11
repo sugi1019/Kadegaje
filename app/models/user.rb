a53170591eb4394371dcf1e1642c1e7714d1c139
EC2 Default User <hisayori.sugimoto@gmail.com>
2025-04-11 01:57:48 +0000

    [Fix] devise メッセージ日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
 
   # ユーザーアイコン
   has_one_attached :users_image
